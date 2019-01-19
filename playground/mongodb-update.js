e91bb5c0e80b67bbd37ec972d7685491a2d88283
Jim Finnell <jimfinnell@jims-mbp.lan>
2019-01-19 14:54:18 -0500

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,38 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if(err){
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID('5c4126b3915ed82a611fb87d')
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    // db.collection('Users').findOneAndUpdate({
+    //     _id: new ObjectID('5c3e8d1755d22a1d26e8b525')
+    // }, {
+    //     $set: {
+    //         name: 'Jim'
+    //     },
+    //     $inc: {
+    //         age: 1
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(JSON.stringify(result, undefined, 2));
+    // });
+
+    // db.close();
+});
